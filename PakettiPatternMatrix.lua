4bb3c384aef8bddb90e73f8005e2f364d46df422
Esa Juhani Ruoho <esaruoho@gmail.com>
2024-05-05 22:28:31 +0300

    Pattern Matrix Menu Entries Tweak
    Switch to Automation
    Bypass EFX (Write to Pattern)
    Enable EFX (Write to Pattern)
    Bypass All Devices on Channel
    Enable All Devices on Channel
    Play at 75% Speed (Song BPM)
    Play at 100% Speed (Song BPM)

--- PakettiPatternMatrix.lua
+++ PakettiPatternMatrix.lua
@@ -1,4 +1,4 @@
--- Show or hide pattern matrix
+-- Show or hide Pattern Matrix
 function showhidepatternmatrix()
   if renoise.app().window.active_middle_frame ~= renoise.ApplicationWindow.MIDDLE_FRAME_PATTERN_EDITOR
     then renoise.app().window.active_middle_frame=renoise.ApplicationWindow.MIDDLE_FRAME_PATTERN_EDITOR 
@@ -14,7 +14,10 @@ end
 renoise.tool():add_keybinding{name="Global:Paketti:Show/Hide Pattern Matrix",invoke=function() showhidepatternmatrix() end}
 renoise.tool():add_menu_entry{name="Pattern Sequencer:Paketti..:Show/Hide Pattern Matrix", invoke=function() showhidepatternmatrix() end}
 renoise.tool():add_menu_entry{name="--Pattern Matrix:Paketti..:Switch to Automation",invoke=function() showAutomation() end}
-
-renoise.tool():add_menu_entry{name="Pattern Matrix:Paketti..:Play at 75% Speed (Song BPM)", invoke=function() playat75() end}
+renoise.tool():add_menu_entry{name="--Pattern Matrix:Paketti..:Bypass EFX (Write to Pattern)", invoke=function() effectbypasspattern()  end}
+renoise.tool():add_menu_entry{name="Pattern Matrix:Paketti..:Enable EFX (Write to Pattern)", invoke=function() effectenablepattern() end}
+renoise.tool():add_menu_entry{name="--Pattern Matrix:Paketti..:Bypass All Devices on Channel", invoke=function() effectbypass() end}
+renoise.tool():add_menu_entry{name="Pattern Matrix:Paketti..:Enable All Devices on Channel", invoke=function() effectenable() end}
+renoise.tool():add_menu_entry{name="--Pattern Matrix:Paketti..:Play at 75% Speed (Song BPM)", invoke=function() playat75() end}
 renoise.tool():add_menu_entry{name="Pattern Matrix:Paketti..:Play at 100% Speed (Song BPM)", invoke=function() returnbackto100()  end}
 
