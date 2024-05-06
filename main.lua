22ff3338160e9f083d315c1cba6801eb4f13c36c
Esa Juhani Ruoho <esaruoho@gmail.com>
2024-05-06 22:14:09 +0300

    move 0g01 + move_up/move_down to other scripts

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
--------------------------------------------------------
 require "Paketti0G01_Loader"
 require "PakettiAutomation"
 require "PakettiControls"
@@ -27,52 +26,12 @@ function startup()
       renoise.app().window:select_preset(1)
       s.sequencer.keep_sequence_sorted=false
       t.groove_enabled=true
-    
 end
-
 
 if not renoise.tool().app_new_document_observable:has_notifier(startup)   
   then renoise.tool().app_new_document_observable:add_notifier(startup)
   else renoise.tool().app_new_document_observable:remove_notifier(startup) end  
-  
 ---------
-function G01()
-local s=renoise.song()
-  local currTrak=s.selected_track_index
-  local currPatt=s.selected_pattern_index
-local rightinstrument=nil
-local rightinstrument=renoise.song().selected_instrument_index-1
-  if not preferences._0G01_Loader.value then return end
-
-local line=s.patterns[currPatt].tracks[currTrak].lines[1]
-    line.note_columns[1].note_string="C-4"
-    line.note_columns[1].instrument_value=rightinstrument
-    s.patterns[currPatt].tracks[currTrak].lines[1].effect_columns[1].number_string="0G"
-    s.patterns[currPatt].tracks[currTrak].lines[1].effect_columns[1].amount_string="01"
-end
-----
-----------------------------------------------------------------------------------------------------------------------------------------
-
---------------------------------------------------------------------------------------------------------------------------------------------------------
-function move_up(chg)
-local sindex=renoise.song().selected_line_index
-local s= renoise.song()
-local note=s.selected_note_column
---This switches currently selected row but doesn't 
---move the note
---s.selected_line_index = (sindex+chg)
--- moving note up, applying correct delay value and moving cursor up goes here
-end
---movedown
-function move_down(chg)
-local sindex=renoise.song().selected_line_index
-local s= renoise.song()
---This switches currently selected row but doesn't 
---move the note
---s.selected_line_index = (sindex+chg)
--- moving note down, applying correct delay value and moving cursor down goes here
-end
---------------------------------------------------------------------------------------------------------------------------------------------------------
 _AUTO_RELOAD_DEBUG = function() startup()
 end
 
